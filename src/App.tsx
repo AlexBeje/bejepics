2680722180b8d514d393419d13ff3c8b368467c6
AlexBeje <alex.bejenaru93@gmail.com>
2023-02-16 23:40:08 +0100

    📖 DOC: Added done and todo tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { Box } from '@mantine/core';
+import { Checkbox, Box } from '@mantine/core';
 import { Header } from './features/1-header/Header';
 
 function App() {
   return (
     <Box className='flex flex-col items-center justify-between h-screen pt-8 pb-2 lg:p-8'>
       <Header />
-      <h1 className='font-light text-4xl lg:text-8xl flex items-center flex-1'>
-        {'< Site Under Construction />'}
-      </h1>
+      <Box className='flex-1  font-extralight flex justify-center flex-col gap-4 sm:min-w-[600px]'>
+        <h1 className='text-4xl lg:text-6xl mb-6 text-center'>
+          {'< Website En Construcción />'}
+        </h1>
+        <Checkbox
+          label='Navegación'
+          size='xl'
+          description='Se ha diseñado y desarrollado la versión desktop y movil del menú.'
+          checked
+        />
+        <Checkbox
+          label='Sección hero'
+          size='xl'
+          description='La sección hero provisional está diseñada y se va a codificar durante los próximos días.'
+          checked={false}
+        />
+      </Box>
     </Box>
   );
 }
