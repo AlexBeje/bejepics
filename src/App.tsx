d9ab37da215f34a5cd06b5adb2dba8fa8e99db0c
AlexBeje <alex.bejenaru93@gmail.com>
2023-02-16 12:52:39 +0100

    Added the base of the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ActionIcon } from '@mantine/core';
+import { IconBrandInstagram } from '@tabler/icons';
+import { ReactComponent as Logo } from '/src/assets/ab-logo--dark.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='flex flex-col items-center justify-between h-screen p-8'>
+      <Logo className='h-32' />
+      <h1 className='font-light text-8xl'>{'< Site Under Construction />'}</h1>
+      <ActionIcon
+        component='a'
+        href='https://www.instagram.com/beje.pics/'
+        size={64}
+        target='_blank'
+      >
+        <IconBrandInstagram size={64} color='black' />
+      </ActionIcon>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
