4b1e7e10ff70ca71efe478489b17998fab50ac66
AlexBeje <abejenaru@opendit.com>
2023-09-21 19:40:21 +0200

    Feat: Refactored the location href logic when accessing home route.

--- app.vue
+++ app.vue
@@ -3,3 +3,22 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+/** Variables **/
+const route = useRoute();
+const indexRoute = ref(route.path === "/");
+
+/** onCreated Lifecycle **/
+const onCreated = () => {
+  if (indexRoute.value) return setPageLayout("white");
+};
+onCreated();
+
+/** onMounterd Lifecycle **/
+onMounted(() => {
+  if (indexRoute.value) {
+    window.location.href = "https://www.instagram.com/beje.pics/";
+  }
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,5 @@
 onMounted(() => {
   // TODO: When site is ready to be published, redirect to home page
   // navigateTo({ path: "/home" });
-  window.location.href = "https://www.instagram.com/beje.pics/";
 });
 </script>
